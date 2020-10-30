60820455899255a53338b192f46a03605c6a66c0
ramironinni <ramironinni@hotmail.com>
2020-10-30 11:07:35 -0300

    Agregado de formularios de registro y login

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
@@ -11,3 +11,12 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
